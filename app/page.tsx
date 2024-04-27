04361ec0fee4a0539bb92ad67dd99748a3034282
daddylunix <ramezjoseph8@gmail.com>
2024-04-27 14:32:31 +0300

    testig

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
+    <h1 className="font-bold text-2xl">Hello World, This is NOT a Subdomain.</h1>
     </>
   );
 }
