eaa1844c5a5b6d3968cf9c9b2197b0497da7c458
daddylunix <ramezjoseph8@gmail.com>
2024-04-27 14:26:11 +0300

    subdomain testing :D

---
+++ middleware.ts
@@ -0,0 +1,38 @@
+// middleware.ts
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+// RegExp for public files
+const PUBLIC_FILE = /\.(.*)$/; // Files
+
+export const getValidSubdomain = (host?: string | null) => {
+    let subdomain: string | null = null;
+    if (!host && typeof window !== 'undefined') {
+      // On client side, get the host from window
+      host = window.location.host;
+    }
+    if (host && host.includes('.')) {
+      const candidate = host.split('.')[0];
+      if (candidate && !candidate.includes('localhost')) {
+        // Valid candidate
+        subdomain = candidate;
+      }
+    }
+    return subdomain;
+  };
+export async function middleware(req: NextRequest) {
+  // Clone the URL
+  const url = req.nextUrl.clone();
+
+  // Skip public files
+  if (PUBLIC_FILE.test(url.pathname) || url.pathname.includes('_next')) return;
+
+  const host = req.headers.get('host');
+  const subdomain = getValidSubdomain(host);
+  if (subdomain) {
+    // Subdomain available, rewriting
+    console.log(`>>> Rewriting: ${url.pathname} to /${subdomain}${url.pathname}`);
+    url.pathname = `/${subdomain}${url.pathname}`;
+  }
+
+  return NextResponse.rewrite(url);
+}
