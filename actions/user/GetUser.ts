8797511dc3781483cd878b68219537a58687c267
daddylunix <ramezjoseph8@gmail.com>
2024-04-28 14:07:25 +0300

    choose-page and multiple actions

---
+++ actions/user/GetUser.ts
@@ -0,0 +1,18 @@
+"use server"
+import prisma from "@/lib/database";
+import { auth } from "@/auth";
+
+export async function GetUser() {
+    const session = await auth();
+    if(!session || session === null) return { error: 'unauthenticated'}
+    try {
+        const user = await prisma.user.findFirst({
+            where: {
+                id: session?.user?.id
+            }
+        })
+        return { user }
+    } catch (error) {
+        return { error }
+    }
+}
