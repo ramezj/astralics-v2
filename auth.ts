770630db92854661fc97a75cdea9b26dd004751b
daddylunix <ramezjoseph8@gmail.com>
2024-04-28 09:27:08 +0300

    we are SOO Back. many new updates

---
+++ auth.ts
@@ -0,0 +1,36 @@
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import NextAuth from "next-auth"
+import GitHub from "next-auth/providers/github"
+import Google from "next-auth/providers/google"
+import prisma from './lib/database';
+ 
+export const { auth, handlers, signIn, signOut } = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+    }),
+  ],
+  secret:process.env.AUTH_SECRET,
+  session: {
+    strategy: 'jwt'
+  },
+  callbacks: {
+    async jwt({ token }) {
+      return token
+    },
+    async session({ token, session }) {
+      if(session.user && token.sub) {
+        session.user.id = token.sub
+      } return session
+    }
+  }
+})
