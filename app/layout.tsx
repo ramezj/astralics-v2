a2fb2eefc239ea152c8b20b42d15119038d8b869
daddylunix <ramezjoseph8@gmail.com>
2024-04-30 14:56:26 +0300

    disableSystem

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-          <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
+          <ThemeProvider attribute="class" defaultTheme="dark" disableTransitionOnChange>
             {children}
           </ThemeProvider>
         </body>
