285be110709e5369293e77a0586848939138c821
daddylunix <ramezjoseph8@gmail.com>
2024-05-04 19:26:14 +0300

    next-themes

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-          <ThemeProvider attribute="class">
+          <ThemeProvider attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange>
             {children}
           </ThemeProvider>
         </body>
