cb269c4589af62a96b434d3a6c22c5f8c319c071
daddylunix <ramezjoseph8@gmail.com>
2024-05-06 12:34:57 +0300

    testing subdomain auth handling

--- auth.ts
+++ auth.ts
@@ -3,11 +3,8 @@ import NextAuth from "next-auth"
 import GitHub from "next-auth/providers/github"
 import Google from "next-auth/providers/google"
 import prisma from './lib/database';
-const useSecureCookies = process.env.NEXTAUTH_URL?.startsWith('https://')
-const cookiePrefix = useSecureCookies ? '__Secure-' : ''
-const hostName = new URL(process.env.NEXTAUTH_URL as string).hostname;
-const rootDomain = "localhost:3000";
- 
+const useSecureCookies = !!process.env.NEXT_URL
+
 export const { auth, handlers, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
   pages: { 
@@ -31,18 +28,18 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
   session: {
     strategy: 'jwt',
   },
-//   cookies: {
-//     sessionToken: {
-//         name: `${useSecureCookies ? "__Secure-" : ""}next-auth.session-token`,
-//         options: {
-//             httpOnly: true,
-//             sameSite: 'lax',
-//             path: '/',
-//             secure: useSecureCookies,
-//             domain: hostName == 'localhost' ? hostName : '.' + rootDomain // add a . in front so that subdomains are included
-//         }
-//     },
-// },
+  cookies: {
+    sessionToken: {
+      name: `${useSecureCookies ? '__Secure-' : ''}next-auth.session-token`,
+      options: {
+        httpOnly: true,
+        sameSite: 'lax',
+        path: '/',
+        domain: '.astralics.com',
+        secure: useSecureCookies,
+      },
+    },
+  },
   trustHost: true,
   callbacks: {
     async jwt({ token }) {
