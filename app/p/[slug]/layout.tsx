6b0ed2b9510bb11f45bac72215c2bea908809d0e
daddylunix <ramezjoseph8@gmail.com>
2024-05-10 16:04:29 +0300

    href to homepage ( temprorary )

--- app/p/[slug]/layout.tsx
+++ app/p/[slug]/layout.tsx
@@ -44,7 +44,9 @@ export default function DashboardLayout({children, params, req} : any) {
       <div className="hidden border-r md:block ">
         <div className="flex h-full max-h-screen flex-col gap-2 sticky top-0">
           <div className="flex h-16 items-center border-b px-4 lg:h-16 lg:px-6 text-left">
+          <Link href='/'>
           <span className="font-bold text-lg">astralics</span>
+          </Link>
           </div>
           <div className="flex-1 ">
             <nav className="grid items-start px-2 text-sm font-medium lg:px-4 gap-2">
