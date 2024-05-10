4168f79b800e2d63f291cd915304d4a65780c573
daddylunix <ramezjoseph8@gmail.com>
2024-05-10 16:03:47 +0300

    simple meta data, new settings page! :D

---
+++ app/p/[slug]/settings/loading.tsx
@@ -0,0 +1,8 @@
+export default function loading() {
+    return (
+        <div className="p-4">
+        <h1 className="font-bold text-3xl">Settings</h1>
+        <p>Loading..</p>
+        </div>
+    )
+}
